ebb92bc56a3943eac0eab5e576e5b124bfbfa8c8
maazz2006ma <maazz2006.ma@gmail.com>
2024-02-21 20:23:41 +0500

    code sync

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <RouterView></RouterView>
+</template>
+
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import LoginCard from './components/LoginCard.vue'
+import { RouterView } from 'vue-router';
+
+</script>
+<style>
+
+
+
+</style>
